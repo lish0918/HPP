f57a3ecb7d491641900c214212a2a4d7e3ac0c9a
lish0918 <chashaeo@gmail.com>
2024-02-27 19:48:48 +0100

    main

--- Project/Sudoku.c
+++ Project/Sudoku.c
@@ -6,85 +6,148 @@
 #define BoxSize 3
 
 int DuplicateNumbersinRow(int board[BoardSize][BoardSize], int x, int num) {
-	for (int y = 0; y < BoardSize; y++) {
-		if (board[x][y] == num) {
-			return 1;
-		}
-	}
-	return 0;
+    for (int y = 0; y < BoardSize; y++) {
+        if (board[x][y] == num) {
+            return 1;
+        }
+    }
+    return 0;
 }
 
 int DuplicateNumbersinCol(int board[BoardSize][BoardSize], int y, int num) {
-	for (int x = 0; x < BoardSize; x++) {
-		if (board[x][y] == num) {
-			return 1;
-		}
-	}
-	return 0;
+    for (int x = 0; x < BoardSize; x++) {
+        if (board[x][y] == num) {
+            return 1;
+        }
+    }
+    return 0;
 }
 
 int DuplicateNumbersinBox(int board[BoardSize][BoardSize], int startX, int startY, int num) {
-	int startRow = startX - startX % BoxSize;
-	int startCol = startY - startY % BoxSize;
-	for (int x = 0; x < BoxSize; x++) {
-		for (int y = 0; y < BoxSize; y++) {
-			if (board[startRow + x][startCol + y] == num) {
-				return 1;
-			}
-		}
-	}
-	return 0;
+    int startRow = startX - startX % BoxSize;
+    int startCol = startY - startY % BoxSize;
+    for (int x = 0; x < BoxSize; x++) {
+        for (int y = 0; y < BoxSize; y++) {
+            if (board[startRow + x][startCol + y] == num) {
+                return 1;
+            }
+        }
+    }
+    return 0;
 }
 
 int ValidateBoard(int board[BoardSize][BoardSize], int x, int y, int num) {
-	if (DuplicateNumbersinRow(board, x, num)) {
-		return 0;
-	}
-	if (DuplicateNumbersinCol(board, y, num)) {
-		return 0;
-	}
-	if (DuplicateNumbersinBox(board, x, y, num)) {
-		return 0;
-	}
-	return 1;
+    if (DuplicateNumbersinRow(board, x, num)) {
+        return 0;
+    }
+    if (DuplicateNumbersinCol(board, y, num)) {
+        return 0;
+    }
+    if (DuplicateNumbersinBox(board, x, y, num)) {
+        return 0;
+    }
+    return 1;
 }
 
 void PrintBoard(int board[BoardSize][BoardSize]) {
-	for (int x = 0; x < BoardSize; x++) {
-		if (x % BoxSize == 0) {
-			printf("-------------------------\n");
-		}
-		for (int y = 0; y < BoardSize; y++) {
-			if (y % BoxSize == 0) {
-				printf("| ");
-			}
-			printf("%d ", board[x][y]);
-		}
-		printf("|\n");
-	}
-	printf("-------------------------\n");
+    for (int x = 0; x < BoardSize; x++) {
+        if (x % BoxSize == 0) {
+            printf("-------------------------\n");
+        }
+        for (int y = 0; y < BoardSize; y++) {
+            if (y % BoxSize == 0) {
+                printf("| ");
+            }
+            printf("%d ", board[x][y]);
+        }
+        printf("|\n");
+    }
+    printf("-------------------------\n");
 }
 
-void GenerateBoard(int board[BoardSize][BoardSize]) {
-	srand(time(NULL));
-	int filledCells = rand() % 10 + 20; // Randomly fill 20-30 cells
-	for (int i = 0; i < filledCells; i++) {
-		int x = rand() % BoardSize;
-		int y = rand() % BoardSize;
-		int num = rand() % BoardSize + 1;
-		if (ValidateBoard(board, x, y, num)) {
-			board[x][y] = num;
-		}
-	}
+// 回溯法解数独
+int SolveSudoku(int board[BoardSize][BoardSize], int row, int col) {
+    if (row == BoardSize - 1 && col == BoardSize) {
+        return 1;
+    }
+
+    if (col == BoardSize) {
+        row++;
+        col = 0;
+    }
+
+    if (board[row][col] != 0) {
+        return SolveSudoku(board, row, col + 1);
+    }
+
+    for (int num = 1; num <= BoardSize; num++) {
+        if (ValidateBoard(board, row, col, num)) {
+            board[row][col] = num;
+            if (SolveSudoku(board, row, col + 1)) {
+                return 1;
+            }
+            board[row][col] = 0;
+        }
+    }
+    return 0;
+}
+
+// 生成唯一解的数独棋盘
+void GenerateUniqueBoard(int board[BoardSize][BoardSize]) {
+    srand(time(NULL));
+    // 清空棋盘
+    for (int i = 0; i < BoardSize; i++) {
+        for (int j = 0; j < BoardSize; j++) {
+            board[i][j] = 0;
+        }
+    }
+
+    // 生成数独棋盘
+    SolveSudoku(board, 0, 0);
+}
+
+// 随机移除数字
+void RemoveNumbers(int board[BoardSize][BoardSize], int cellsToRemove) {
+    srand(time(NULL));
+    while (cellsToRemove > 0) {
+        int x = rand() % BoardSize;
+        int y = rand() % BoardSize;
+        if (board[x][y] != 0) {
+            board[x][y] = 0;
+            cellsToRemove--;
+        }
+    }
+}
+
+void WriteBoardToFile(int board[BoardSize][BoardSize], const char *filename) {
+    FILE *file = fopen(filename, "w");
+    if (file == NULL) {
+        printf("Error opening file.\n");
+        return;
+    }
+
+    for (int i = 0; i < BoardSize; i++) {
+        for (int j = 0; j < BoardSize; j++) {
+            fprintf(file, "%d ", board[i][j]);
+        }
+        fprintf(file, "\n");
+    }
+
+    fclose(file);
+    printf("Board written to file %s\n", filename);
 }
 
 int main() {
-	printf("Welcome to Sudoku!\n");
-	printf("Generating the Sudoku board...\n");
+    printf("Welcome to Sudoku!\n");
+    printf("Generating the Sudoku board...\n");
 
-	int board[BoardSize][BoardSize] = {0};
-	GenerateBoard(board);
-	PrintBoard(board);
+    int board[BoardSize][BoardSize] = {0};
+    GenerateUniqueBoard(board);
+    // 随机移除一些数字
+    int cellsToRemove = rand() % 30 + 31; // 31-60 cells to remove
+    RemoveNumbers(board, cellsToRemove);
+    PrintBoard(board);
+    WriteBoardToFile(board, "sudoku_board.txt");
 
-	return 0;
+    return 0;
 }
---
+++ Project/Sudoku_Solver.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+
+#define BoardSize 9
+
+int DuplicateNumbersinRow(int board[BoardSize][BoardSize], int x, int num) {
+    for (int y = 0; y < BoardSize; y++) {
+        if (board[x][y] == num) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int DuplicateNumbersinCol(int board[BoardSize][BoardSize], int y, int num) {
+    for (int x = 0; x < BoardSize; x++) {
+        if (board[x][y] == num) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int DuplicateNumbersinBox(int board[BoardSize][BoardSize], int startRow, int startCol, int num) {
+    for (int x = 0; x < 3; x++) {
+        for (int y = 0; y < 3; y++) {
+            if (board[startRow + x][startCol + y] == num) {
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+int ValidateBoard(int board[BoardSize][BoardSize], int x, int y, int num) {
+    if (DuplicateNumbersinRow(board, x, num) || DuplicateNumbersinCol(board, y, num) ||
+        DuplicateNumbersinBox(board, x - x % 3, y - y % 3, num)) {
+        return 0;
+    }
+    return 1;
+}
+
+int Solve(int board[BoardSize][BoardSize], int unAssignInd[], int N_unAssign) {
+    if (N_unAssign == 0) {
+        // No more empty positions, solution found
+        return 1;
+    }
+    int index = unAssignInd[N_unAssign - 1];
+    int x = index / BoardSize;
+    int y = index % BoardSize;
+    for (int val = 1; val <= BoardSize; val++) {
+        if (ValidateBoard(board, x, y, val)) {
+            board[x][y] = val; // Set guess
+            // Solve recursively
+            if (Solve(board, unAssignInd, N_unAssign - 1)) {
+                return 1;
+            }
+            // If the recursive call didn't return a solution, reset the current cell and try the next value
+            board[x][y] = 0; // Reset the value for backtracking
+        }
+    }
+    return 0;
+}
+
+void ReadBoardFromFile(int board[BoardSize][BoardSize], int unAssignInd[], int *N_unAssign) {
+    // Read sudoku board from file
+    FILE *file = fopen("sudoku_board.txt", "r");
+    if (file == NULL) {
+        printf("Failed to open file.\n");
+        return;
+    }
+
+    *N_unAssign = 0; // Initialize the number of unassigned cells
+
+    for (int x = 0; x < BoardSize; x++) {
+        for (int y = 0; y < BoardSize; y++) {
+            if (fscanf(file, "%d", &board[x][y]) != 1) {
+                printf("Failed to read board from file.\n");
+                fclose(file);
+                return;
+            }
+            if (board[x][y] == 0) {
+                unAssignInd[(*N_unAssign)++] = x * BoardSize + y; // Increment the count of unassigned cells
+            }
+        }
+    }
+
+    fclose(file);
+}
+
+void PrintBoard(int board[BoardSize][BoardSize]) {
+    printf("-------------------------\n");
+    for (int x = 0; x < BoardSize; x++) {
+        printf("| ");
+        for (int y = 0; y < BoardSize; y++) {
+            printf("%d ", board[x][y]);
+            if ((y + 1) % 3 == 0) {
+                printf("| ");
+            }
+        }
+        printf("\n");
+        if ((x + 1) % 3 == 0) {
+            printf("-------------------------\n");
+        }
+    }
+}
+
+int main() {
+    int board[BoardSize][BoardSize];
+    int unAssignInd[BoardSize * BoardSize];
+    int N_unAssign = 0;
+
+    ReadBoardFromFile(board, unAssignInd, &N_unAssign);
+
+    if (Solve(board, unAssignInd, N_unAssign)) {
+        // Print solved board
+        PrintBoard(board);
+    } else {
+        printf("No solution found.\n");
+    }
+
+    return 0;
+}
